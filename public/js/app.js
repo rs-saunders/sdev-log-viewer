8f773560e9b170c4c3bd418e80c57525cda58c6c
Richard Saunders <richard@sdevsolutions.com>
2015-11-01 22:35:18 +0000

    load file contents in frontend
    - ajax request to /logs/:file to get last 50 lines
    - loads in first log file by default

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 
 $(function() {
 
+    var $logLines = $('#log-lines');
+
     /**
      * Populate list of log files
      */
@@ -16,17 +18,24 @@ $(function() {
                 var $li = $('<li></li>')
                     .append($a);
 
-                if(index === 0) {
-                    $li.addClass('active');
-                }
-
                 $('ul.nav-sidebar')
                     .append($li);
+
+                //activate the first log file
+                if(index === 0) {
+                    $li.addClass('active');
+                    $('a', $li).trigger('click');
+                }
             });
 
+            $logLines.empty();
         })
         .fail(function(data) {
-            console.log(data);
+            var $li = $('<p></p>')
+                .addClass('bg-danger')
+                .text(data);
+
+            $logLines.empty().append($li);
         });
 
     /**
@@ -35,7 +44,32 @@ $(function() {
     $('ul.nav-sidebar').on('click', 'li', function(event) {
         event.preventDefault();
         $(this).addClass('active').siblings().removeClass('active');
-        console.log($('a', this).attr('href'));
+
+        var path = $('a', this).attr('href');
+
+        $logLines.empty();
+
+        /**
+         * request the log file contents and load into the page
+         */
+        $.ajax(path + '?n=50')
+            .done(function(data) {
+
+                //wrap each line in an <li> tag
+                data.split("\n").forEach(function(line) {
+                    if(line) $logLines.append($('<li></li>').text(line))
+                });
+
+                //scroll to the bottom of the page
+                $('html, body').scrollTop( $(document).height() - $(window).height() );
+            })
+            .fail(function(data) {
+                var $li = $('<p></p>')
+                    .addClass('bg-danger')
+                    .text(data);
+
+                $logLines.empty().append($li);
+            });
     })
 
 });
